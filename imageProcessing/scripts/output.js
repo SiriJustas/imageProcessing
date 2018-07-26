74a257dce00a24eed2d9c8db83a7e0171d9845b8
Justas <40943117+SiriJustas@users.noreply.github.com>
2018-07-26 10:31:14 +0300

    Update output.js

--- imageProcessing/scripts/output.js
+++ imageProcessing/scripts/output.js
@@ -1,12 +1,8 @@
-
-function output(imgData){
+function output(imgData,ct){
 	var imagedata = imgData;
 	var pav = document.getElementsByTagName("img")[0];
 	width = pav.naturalWidth;
 	height = pav.naturalHeight;
-	var canvass2 = document.getElementById("canvasas2");
-	var ctx2 = canvass2.getContext("2d");
-	canvass2.width = width;
-	canvass2.height = height;
-	ctx2.putImageData(imagedata, 0, 0);
+	var ctx = ct;
+	ctx.putImageData(imagedata, 0, 0);
 }
